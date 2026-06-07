389d7fd6be7504d1b140e06cd6cfc8251a7e6efe
antz24 <ajdamaso07@gmail.com>
2026-06-07 12:57:02 -0400

    Added basic variables in JS

---
+++ assignmentOneJS/js/script.js
@@ -0,0 +1,12 @@
+const agentBtn = document.querySelector("agent");
+const locationBtn = document.querySelector("location");
+const loadoutBtn = document.querySelector("loadout");
+const objectiveBtn = document.querySelector("objective");
+const riskBtn = document.querySelector("risk");
+const output = document.querySelector("output");
+
+let agentList = ["Ghost", "Shadow", "Viper", "Nova"];
+let locationList = ["Exultia", "Mars Core", "Nekravol", "Urdak"];
+let weaponList = ["Combat Shotgun", "Heavy Assault Rifle", "Plasma Rifle", "Ballista"];
+let objectiveList = ["Rescue Hostage", "Retrieve Briefcase", "Sabotage Supply Chain"];
+let riskList = ["Low", "Medium", "High", "Very High"];
