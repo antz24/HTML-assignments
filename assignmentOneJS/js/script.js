bf391fcbd53321476e93cbe5583abd756f2ae1f2
antz24 <ajdamaso07@gmail.com>
2026-06-07 13:29:20 -0400

    Added counters to other buttons
    also fixed typo in riskOut var and changed agentList contents

--- assignmentOneJS/js/script.js
+++ assignmentOneJS/js/script.js
@@ -4,28 +4,33 @@ let agentCounter = 0;
 
 const locationBtn = document.querySelector("#location");
 const locationOut = document.querySelector("#outputLocation");
+let locationCounter = 0;
 
 const loadoutBtn = document.querySelector("#loadout");
 const loadoutOut = document.querySelector("#outputLoadout");
+let loadoutCounter = 0;
 
 const objectiveBtn = document.querySelector("#objective");
 const objOut = document.querySelector("#outputObj");
+let objCounter = 0;
 
 const riskBtn = document.querySelector("#risk");
-const riskOUt = document.querySelector("#outputRisk");
+const riskOut = document.querySelector("#outputRisk");
+let riskCounter = 0;
 
 const briefingBtn = document.querySelector("#briefing");
 const briefingOut = document.querySelector("#outputBriefing");
 
 
 
-let agentList = ["Ghost", "Shadow", "Viper", "Nova"];
+let agentList = ["Doom Slayer", "Samuel Hayden", "Commander Thira", "King Novik"];
 let locationList = ["Exultia", "Mars Core", "Nekravol", "Urdak"];
 let weaponList = ["Combat Shotgun", "Heavy Assault Rifle", "Plasma Rifle", "Ballista"];
 let objectiveList = ["Rescue Hostage", "Retrieve Briefcase", "Sabotage Supply Chain"];
 let riskList = ["Low", "Medium", "High", "Very High"];
 
 agentBtn.addEventListener("click", agentCycle);
+locationBtn.addEventListener("click", locationCycle);
 
 function agentCycle()
 {
@@ -40,4 +45,9 @@ function agentCycle()
         agentCounter++;
     }
     agentOut.textContent = agentList[agentCounter];
+}
+
+function locationCycle()
+{
+
 }
