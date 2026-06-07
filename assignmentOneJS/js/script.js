ac80e3fcc71e64409ac809bdb40e199e31ad0f04
antz24 <ajdamaso07@gmail.com>
2026-06-07 13:24:24 -0400

    Added agent cycling functionality
    additionally fixed querySelector issues

--- assignmentOneJS/js/script.js
+++ assignmentOneJS/js/script.js
@@ -1,20 +1,21 @@
-const agentBtn = document.querySelector("agent");
-const agentOut = document.querySelector("outputAgent");
+const agentBtn = document.querySelector("#agent");
+const agentOut = document.querySelector("#outputAgent");
+let agentCounter = 0;
 
-const locationBtn = document.querySelector("location");
-const locationOut = document.querySelector("outputLocation");
+const locationBtn = document.querySelector("#location");
+const locationOut = document.querySelector("#outputLocation");
 
-const loadoutBtn = document.querySelector("loadout");
-const loadoutOut = document.querySelector("outputLoadout");
+const loadoutBtn = document.querySelector("#loadout");
+const loadoutOut = document.querySelector("#outputLoadout");
 
-const objectiveBtn = document.querySelector("objective");
-const objOut = document.querySelector("outputObj");
+const objectiveBtn = document.querySelector("#objective");
+const objOut = document.querySelector("#outputObj");
 
-const riskBtn = document.querySelector("risk");
-const riskOUt = document.querySelector("outputRisk");
+const riskBtn = document.querySelector("#risk");
+const riskOUt = document.querySelector("#outputRisk");
 
-const briefingBtn = document.querySelector("briefing");
-const briefingOut = document.querySelector("outputBriefing");
+const briefingBtn = document.querySelector("#briefing");
+const briefingOut = document.querySelector("#outputBriefing");
 
 
 
@@ -29,4 +30,14 @@ agentBtn.addEventListener("click", agentCycle);
 function agentCycle()
 {
     
+
+    if (agentCounter === agentList.length - 1)
+    {
+        agentCounter = 0;
+    }
+    else
+    {
+        agentCounter++;
+    }
+    agentOut.textContent = agentList[agentCounter];
 }
